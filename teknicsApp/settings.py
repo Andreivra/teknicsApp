a28316254573bc652b74cb81c234eb88a09ac935
Andrei <vrincean_andrei@yahoo.com>
2023-07-16 08:56:13 +0300

    15.07 - Mod design form

--- teknicsApp/settings.py
+++ teknicsApp/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/homepage/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
